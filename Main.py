cc1f60423df5fff67223eae5c3d3ee85595ca841
joaolucasbarbosa <jlucasbarbosa02@gmail.com>
2024-01-20 16:32:30 -0300

    Added customize screen

--- Main.py
+++ Main.py
@@ -103,7 +103,7 @@ def options():
         
         #Definição dos botões
 
-        OPTIONS_BACK = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 650), 
+        OPTIONS_BACK = Button(image=None, pos=(640, 650), 
                             text_input="BACK", font=get_font(40), base_color="Black", hovering_color="Green")
         
         SOUND_BUTTON = Button(image=None, pos=(640, 250),
@@ -152,6 +152,40 @@ def options():
 
         pygame.display.update()
 
+def customize():
+
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(45).render("Customize Ship", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        BACK_BUTTON = Button(image=None, pos=(640, 650), 
+        text_input="BACK", font=get_font(40), base_color="White", hovering_color="Green")
+
+        BACK_BUTTON.changeColor(PLAY_MOUSE_POS)
+        BACK_BUTTON.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if BACK_BUTTON.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    paused = True
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        # Incrementa o contador quando a barra de espaço é pressionada
+                        counter += 1 
+
+        pygame.display.update()
+
 def main_menu():
     global is_sound_enabled
     global is_hard_mode
@@ -164,16 +198,18 @@ def main_menu():
         MENU_TEXT = get_font(45).render("Luis Inacio Lula da Silva", True, "#b68f40")
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), 
-                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="Green")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), 
-                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="Green")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550), 
-                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="Green")
+        PLAY_BUTTON = Button(image=None, pos=(640, 250), 
+                            text_input="PLAY", font=get_font(55), base_color="#d7fcd4", hovering_color="Green")
+        CUSTOMIZE_BUTTON = Button(image=None, pos=(640, 350), 
+                            text_input="CUSTOMIZE", font=get_font(55), base_color="#d7fcd4", hovering_color="Green")
+        OPTIONS_BUTTON = Button(image=None, pos=(640, 450), 
+                            text_input="OPTIONS", font=get_font(55), base_color="#d7fcd4", hovering_color="Green")
+        QUIT_BUTTON = Button(image=None, pos=(640, 650), 
+                            text_input="QUIT", font=get_font(55), base_color="#d7fcd4", hovering_color="Green")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
-        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON, CUSTOMIZE_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
         
@@ -186,6 +222,8 @@ def main_menu():
                     play(is_hard_mode)
                 if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
                     options()
+                if CUSTOMIZE_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    customize()
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
